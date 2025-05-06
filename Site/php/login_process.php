932f0aa1653f81d0a842826a2375d700d864daaf
Nureddin Can Erdeğer <142605860+nureddincan@users.noreply.github.com>
2025-05-06 21:10:34 +0300

    Update login_process.php

--- Site/php/login_process.php
+++ Site/php/login_process.php
@@ -1,6 +1,9 @@
 <?php
 // Formdan gelen verileri al
-$username = isset($_POST['username']) ? $_POST['username'] : ''; // 
+
+// Eğer POST ile 'username' verisi geldiyse $username değişkenine ata, yoksa boş string ata
+$username = isset($_POST['username']) ? $_POST['username'] : ''; 
+// Eğer POST ile 'password' verisi geldiyse $password değişkenine ata, yoksa boş string ata
 $password = isset($_POST['password']) ? $_POST['password'] : '';
 
 // Doğru bilgiler
@@ -30,13 +33,14 @@ if ($username === $dogru_kullanici && $password === $dogru_sifre) {
     <head>
         <meta charset='UTF-8'>
         <title>Giriş Başarısız</title>
-        <meta http-equiv='refresh' content='3;url=/proje/login.html'>
+        <meta http-equiv='refresh' content='3;url=/B231210041_WebTeknolojileri_Proje/login.html'>
         <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css' rel='stylesheet'>
     </head>
     <body class='d-flex justify-content-center align-items-center vh-100 bg-danger text-white'>
         <div class='text-center'>
             <h1>Hatalı Giriş</h1>
             <p>3 saniye içinde giriş sayfasına yönlendirileceksiniz...</p>
+            <p><a href='/B231210041_WebTeknolojileri_Proje/login.html' class='text-white'>Eğer yönlendirilmezseniz buraya tıklayın</a></p>
         </div>
     </body>
     </html>";
